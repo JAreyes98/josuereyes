379e5ee15cd1241aeaeaf14d403496fc758692a5
JAreyes98 <josuereyeschevy@gmail.com>
2022-11-17 23:27:40 +0100

    Documentacion

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # PRIMERA EVALUACIÓN CURSO DE SPRING
+
+```
+mvn clean package -Pproduccion -Dusuario=UCEM_IRENE -Dclave=1234 -Dusrapi=user -Dclsapi=1234
+```
--- src/main/java/com/josuereyes/prueba/web/ClienteController.java
+++ src/main/java/com/josuereyes/prueba/web/ClienteController.java
@@ -2,6 +2,8 @@ package com.josuereyes.prueba.web;
 
 import com.josuereyes.prueba.model.Cliente;
 import com.josuereyes.prueba.service.ClienteService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,30 +22,37 @@ public class ClienteController {
         this.service = service;
     }
 
+    @ApiOperation("Retorna todos los registros de clientes")
     @GetMapping("/")
     public ResponseEntity<List<Cliente>> all() {
         return ResponseEntity.ok(service.obtenerTodos());
     }
 
+    @ApiOperation("Retorna el cliente con id")
     @GetMapping("/{id}")
-    public ResponseEntity<Cliente> byId(@PathVariable String id) {
+    public ResponseEntity<Cliente> byId(@ApiParam("Identificador del cliente, Tipo <b>Integer<b/>") @PathVariable String id) {
         return service.obtenerPorId(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
 
+
+    @ApiOperation("Registra un nuevo cliente que posee el formato: ")
     @PostMapping("/new")
-    public ResponseEntity<Cliente> nuevo(@RequestBody Cliente cliente) {
+    public ResponseEntity<Cliente> nuevo(@ApiParam("Cliente a ser guardado") @RequestBody Cliente cliente) {
         service.nuevo(cliente);
         return ResponseEntity.ok(cliente);
     }
 
+    @ApiOperation("Actualiza un cliente que posee el formato: ")
     @PutMapping("/update")
-    public ResponseEntity<Cliente> update(@RequestBody Cliente cliente) {
+    public ResponseEntity<Cliente> update(@ApiParam("Cliente a ser actualizado") @RequestBody Cliente cliente) {
         service.actualizar(cliente);
         return ResponseEntity.ok(cliente);
     }
 
+
+    @ApiOperation("Elimina un cliente con id")
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<String> update(@PathVariable String id) {
+    public ResponseEntity<String> update(@ApiParam("Id de Cliente a ser eliminado") @PathVariable String id) {
         service.eliminar(id);
         Optional<Cliente> cl = service.obtenerPorId(id);
         return cl.map(f -> ResponseEntity.ok("ELIMINADO")).orElse(ResponseEntity.badRequest().body("NO SE ELIMINO"));
